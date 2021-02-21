620c3fe723c2c3011774e37b24289a46132bc8e9
renauldie <renauldizanagi@gmail.com>
2021-02-21 09:32:29 +0700

    create update user !

---
+++ routes/handler/users/update.js
@@ -0,0 +1,72 @@
+const { User } = require('../../../models');
+const Validator = require('fastest-validator');
+const v = new Validator();
+
+module.exports = async (req, res) => {
+	const schema = {
+		email: 'string|optional',
+		phone: 'string|optional',
+		avatar: 'string|optional',
+		city_address: 'string|optional',
+		tft: 'boolean|optional',
+		ipk: 'number|optional',
+	};
+
+	const validate = v.validate(req.body, schema);
+	if (validate.length) {
+		return res.status(400).json({
+			status: 'error',
+			message: validate,
+		});
+	}
+
+	const id = req.params.id;
+	const user = await User.findByPk(id);
+	if (!user) {
+		return res.status(404).json({
+			status: 'error',
+			message: 'user not found',
+		});
+	}
+
+  const email = req.body.email;
+  if (email) {
+    const checkEmail = await User.findOne({
+      where: { email }
+    });
+
+    if (checkEmail && email !== user.email) {
+      return res.status(409).json({
+        status: 'error',
+        message: 'email already exist'
+      })
+    }
+  }
+
+  const {
+    phone, avatar, city_address, tft, ipk
+  } = req.body
+
+	await user.update({
+		email,
+		phone,
+		avatar,
+		city_address,
+		tft,
+		ipk,
+	});
+
+  return res.json({
+    status: 'success',
+    data: {
+      id: user.id,
+      name: user.name,
+      email,
+      phone,
+      avatar,
+      city_address,
+      tft,
+      ipk
+    }
+  })
+};
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const usersHandler = require('./handler/users');
 
 router.post('/login', usersHandler.login);
+router.put('/:id', usersHandler.update);
 
 module.exports = router;
