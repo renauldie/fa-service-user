92e39ec5a2483927d4361d2d97544593f57295da
renauldie <renauldizanagi@gmail.com>
2021-02-21 09:30:49 +0700

    add phone

---
+++ models/User.js
@@ -0,0 +1,66 @@
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define(
+		'User',
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			npm: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			study_program_id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			email: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			phone: {
+				type: DataTypes.DOUBLE,
+				allowNull: false,
+				field: 'phone'
+			},
+			avatar: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			city_address: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			tft: {
+				type: DataTypes.BOOLEAN,
+				defaultValue: false,
+			},
+			ipk: {
+				type: DataTypes.DECIMAL,
+				allowNull: true,
+			},
+			createdAt: {
+				field: 'created_at',
+				type: DataTypes.DATE,
+				allowNull: false,
+			},
+			updatedAt: {
+				field: 'updated_at',
+				type: DataTypes.DATE,
+				allowNull: false,
+			},
+		},
+		{
+			tableName: 'users',
+			timestamps: true,
+		}
+	);
+
+	return User;
+};
